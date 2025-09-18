3cd1ad5bcedc5a910831b0ca609e91fb4d71a9dd
Himans <himansbhardwaj1566@gmail.com>
2025-09-18 13:30:06 +0530

    Add api routes(register,login,logout), add nodemailer config file

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -5,13 +5,15 @@ import userModel from "../models/userModel.js";
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
+  // check we have all three credintial from user
   if (!name || !email || !password) {
     return res.json({ success: false, message: "Missing Details" });
   }
   try {
-    const exisingUser = await userModel.findOne({ email });
+    const existingUser = await userModel.findOne({ email });
 
-    if (exisingUser) {
+    // check if user is already exists or not
+    if (existingUser) {
       return res.json({ success: false, message: "User already exists" });
     }
 
@@ -20,8 +22,71 @@ export const register = async (req, res) => {
     const user = new userModel({ name, email, password: hashedPassword });
     await user.save();
 
-    const token = jwt.sign({ id: user._id });
+    // Generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ success: true });
   } catch (error) {
     res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Email and Password are required",
+    });
+  }
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "Invalid email" });
+    }
+
+    // compare user's given passwaord with saved password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ success: true });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    return res.json({ success: true, message: "Logged Out" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
   }
 };
