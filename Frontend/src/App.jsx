2c08d87a0d912397bd55a501c5ef676314562f4f
Himans <himansbhardwaj1566@gmail.com>
2025-09-22 18:47:04 +0530

    add frontend pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/emailVerify" element={<EmailVerify />} />
+        <Route path="/resetPassword" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
